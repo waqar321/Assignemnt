8167bd8ec37ec9dc7d35681b1bb934034b4a17c5
waqar321 <waqarmughal707@gmail.com>
2017-04-15 11:22:57 +0500

    Assignment, Chapter 3 and 4

---
+++ Completed_Assignment.py
@@ -0,0 +1,307 @@
+# print("\n=====================Example 3.1=================================\n\n")
+# agreed=[]
+# not_agree=[]
+# names = ["waqar", "kashif", "imran", "usman", "bijjar"]
+# i=0
+# print("Here Is List Of Stored Members : \n")
+# for accessing in names:
+	# i=i+1
+	# print("Person: :" + str(i) + "\t" + accessing.title())
+
+
+# print("\n=====================Example 3.2 =================================\n\n")
+	
+# for accessing in names:
+	
+	# userinput = input("Would you like to come to have Dinner: Mr. " + accessing.title() + ":\t")
+	
+	# if userinput == 'YES' or userinput == 'yes':
+		# yes = accessing
+		# agreed.append(yes)
+		# print(yes.title() + "\tis agree\t\n")
+		
+	# elif userinput == 'NO' or userinput == 'no':
+		# no = accessing
+		# not_agree.append(str(no))
+		# print(no.title() + "\t is not agree\t\n")
+		
+	# else:
+		# print("You did'nt answered correct Make sure answer only in Yes or no ")
+
+
+# print("Finally Invites list are \n")
+
+# for agreed in agreed:
+	# print("Agreed Persons is: " + "\t" + agreed)
+
+# for not_agree in not_agree:
+	# print("Not Agreed Persons is: " + "\t" + not_agree)
+
+# print("\n=====================Example 3.3=================================\n\n")
+
+# vehicle= ["Corolla", "Civic", "BMW", "Sherade", "Honda", "Atlas"]
+# n=len(vehicle)
+# print("These Type of vehicles Are Available:\n ")
+# j=0;
+# for i in range(0, n):
+	# j=j+1 
+	# print(str(j) + " = " + vehicle[i])
+
+# print("\nWhich one Would You Like to choose!\n\n")
+# print("Enter: \n 1 For Corolla \t 2 For Civic \t 3 For BMW \t 4 For Sherade \n 5 For Honda \t 6 For Atlas \n ")
+# j=j+1
+
+# userinput = int(input("Enter Your Choice To Get Vehile:\t"))
+	
+# if userinput == 1:
+	# print("You have selected: \t" + vehicle[0] + "\n")
+# elif userinput == 2:
+	# print("You have selected: \t" + vehicle[1] + "\n")
+# elif userinput == 3:
+	# print("You have selected: \t" + vehicle[2] + "\n")
+# elif userinput == 4:
+	# print("You have selected: \t" + vehicle[3] + "\n")
+# elif userinput == 5:
+	# print("You have selected: \t" + vehicle[4] + "\n")
+# elif userinput == 6:
+		# print("You have selected: \t" + vehicle[5] + "\n")
+	
+	
+	
+# print("=====================Example 3.4=================================\n")
+
+# message = " you are invited for dinner"
+# guestList = ['Rehan','Aslam','Mughal','Kashif']
+# n = len(guestList)
+# for i in range(0, n):
+   # print ("\n Mr. " + guestList[i] + message.title() + "\tThank you !")
+   
+	
+	
+# print("\n=====================Example 3.5=================================\n")
+
+# guestList = ['Saad','Osama','Sohaib','Abdul']
+
+# guestList.insert(guestList.index('Osama'),'Pasha')
+# guestList.remove('Osama')
+# for i in guestList:
+   # print ("\n Mr. " + i + message.title() + "\tThank you !")
+   # print('-------------------------------------------------------')
+
+# print("\n=====================Example 3.6=================================\n")
+
+# message1 = "found a bigger dinner table."
+#####mid = len(guestList)/2
+
+# print(message1.title()+"\n")
+# guestList.insert(0,'Dilawar')
+# guestList.insert((len(guestList)/2).__int__(),'Khan')
+# guestList.append('waqar')
+
+# for show in guestList:
+	# print("Finally Invitations are: " + show)
+
+# print("\n=====================Example 3.7=================================\n")
+
+# while(len(guestList)>2):
+    # a = guestList.pop()
+    # print(" Sorry Mr. " + a.title() + "I can not invite you to dinner".title()+"\n")		
+
+# for j in guestList:
+	# print("\n You Guyx are Still Invited  \t" + 	j)
+
+# for i in guestList:
+    # del guestList[0:2]
+
+# if(len(guestList)==0):
+	# print('List is empty')
+	
+# print("\n=====================Example 3.8=================================\n")
+
+# world = ["America", "Pakistan", "Saudi Arabia", "India", "Norway"]
+
+# print(world)
+# print(sorted(world))
+# print(world)
+# world.reverse()
+# print(world)
+# world.reverse()
+# print(world)
+# world.sort()
+# print(world)
+# world.sort(reverse=True)
+# print(world)
+
+# print("\n=====================Example 3.9=================================\n")
+
+# print(str(n) + "\tNumber of people were invited")
+
+# print("\n=====================Example 3.10=================================\n")
+
+# think_list = ["rivers", "etc", "wiki", "changed"]
+# think_list.sort()
+# print(think_list)
+# think_list.reverse()
+# print(think_list)
+# str(think_list)
+# print(think_list)
+# len(think_list)
+# print(think_list)
+# think_list.remove("etc")
+# print(think_list)
+# think_list.pop()
+# print(think_list)
+# del think_list[1]
+# print(think_list)
+# think_list.insert(2, "wiki mughal")
+# print(think_list)
+
+# print("\n============ Chapter 3 The End ============")
+# print("\n ========================================================================")
+
+# print("\n============ Chapter 4 The Start ============")
+
+
+# print("====================== example 4.1==========================")
+
+# print("Here are some list of pizzas\n")
+# pizzas = ["Chicken Tikka", "Ajeeb Tikka", "Italian Crust", "Afghani Pizz", 'Margherita', "Ajeeb Pizza"]
+
+# for listing in pizzas:
+	# print(listing.title()+ ":\n" +  "Is one of the best pizza for them who like it =P")
+
+# print("\n\nActually i did'nt tasted that pizza so \nI have not any word to say  something =P \nBut i have wrote 2 or 3 line to complete my task requirement =P ")
+
+
+# print("\n====================== example 4.2==========================\n")
+
+# Animals = ['Dog', "Cat", "Tiger"]
+# for animal in Animals:
+	# print(animal + "\t Runs Fast & Eat Meat & Know Well To Care Of Children\n")
+# print(Animals[0] + " is expert to smell")
+# print(Animals[1] + "is expert to find food")
+# print(Animals[2] + "is expert to Hunting")
+
+# print("\n====================== example 4.3==========================\n")
+
+# for i in range(0, 20):
+	# print(i)
+
+# print("\n====================== example 4.4==========================\n")
+
+# squares = []
+# for value in range(0, 1000000):
+	# square = value
+	# squares.append(square)
+# print("appended")
+
+# print("\n====================== example 4.5==========================\n")
+
+# min1 = min(squares)
+# print("minimum Number: " + str(min1))
+# max1= max(squares)
+# print("Maximum Number: " + str(max1))
+# sum1 = sum(squares)
+# print("Total Sum is : " + str(sum1))
+	
+# print("\n====================== example 4.5==========================\n")
+
+# even_numbers = list(range(1,21,3))
+# print(even_numbers)
+
+# print("\n====================== example 4.7==========================\n")
+
+# squares1 = []
+# for value in range(3, 30):
+	# square = value*3
+	# squares1.append(square)
+# for value1 in squares1:
+	# print(value1)
+
+
+
+# print("\n====================== example 4.8==========================\n")
+
+# squares2 = []
+# for value in range(1, 10):
+	# square = value**2
+	# squares2.append(square)
+# for value1 in squares2:
+	# print(value1)
+	
+# print("\n====================== example 4.9==========================\n")
+
+# squares1 = [value*3 for value in range(1,11)]
+# print(squares1)
+
+# print("\n====================== example 4.10==========================\n")
+
+# myList = ['check', 'check1', 'check2', 'check3', 'check4', 'check5', 'check6']
+# print("First 3 elements in the list are:")
+# print(myList[:3])
+# print("3 elements from the middle of the list are:")
+# listLength = len(myList)
+# if listLength % 2 == 0:
+    # print(myList[int(listLength / 2 - 1.5):int(listLength / 2 + 1.5)])
+# else:
+    # print(myList[int(listLength / 2 - 1.5):int(listLength / 2 + 1.5)])
+# print("last 3 elements in the list are:")
+# print(myList[-3:])
+
+# print("\n====================== example 4.11==========================\n")
+
+# pizzas = ["Chicken Tikka", "Italian Crust", "Afghani Pizza"]
+# copy_pizzas = pizzas[:]
+# pizzas.insert(0, 'Fajita')
+# copy_pizzas.insert(0, 'Chicken Golden')
+# if pizzas == copy_pizzas:
+    # print("Both lists are same.")
+# else:
+    # print("Both lists are not same")
+
+# print("My favorite pizzas are:")
+# for pizza in pizzas:
+    # print(pizza)
+
+# print("My friend’s favorite pizzas are")
+# for copy_pizza in copy_pizzas:
+    # print(copy_pizza)
+	
+# print("\n====================== example 4.12==========================\n")
+	
+
+# print("Pizzas are:")
+# for pizza in pizzas:
+    # print(pizza)
+
+
+# print("Animals are")
+# for animal in Animals:
+    # print(animal)
+
+# for myList1 in myList:
+    # print(myList1)
+
+print("\n============ Chapter 4 The End ============")
+print("\n ========================================================================")
+
+
+# Problem #1:
+
+num = 0
+for i in range(1,1000):
+    if i%3 == 0 or i%5 == 0:
+        num = num+i
+        print(num)
+
+# problem #2:
+
+numbers = [1,2]
+sum = 0
+for i in range(4000000):
+    if i == numbers[-1] +numbers[-2]:
+        numbers.append(i)
+        for n in numbers:
+            if n%2 == 0:
+                sum = sum + n
+                print(sum)
